58dbfa477e6002d827a0f118533f2ac1aa4105a4
theo-js <dev.js.theo@gmail.com>
2021-08-03 22:49:37 +0200

    Change theme-color meta tag, write unit test for setVariableInterval function

--- src/utils/tests/time.test.ts
+++ src/utils/tests/time.test.ts
@@ -17,13 +17,14 @@ describe('time module', () => {
         expect(minsToHrsMins(5000)).toBe('83:20')
     })
 
-    /*test('setVariableInterval', () => {
+    test('setVariableInterval', () => {
         // Mock callback
         const callback = jest.fn()
 
         // Get next interval callback
         let nextInterval = 0
         function * genNextInterval () {
+            // Interval is 1 second longer everytime
             while (true) {
                 yield nextInterval += 1000
             }
@@ -35,12 +36,48 @@ describe('time module', () => {
         const clearInterval = setVariableInterval(callback, getNextInterval)
         expect(callback).not.toBeCalled()
 
-        // Stop after 12 seconds
-        setTimeout(clearInterval, 12000)
+        // Stop after 10 seconds
+        setTimeout(clearInterval, 10000)
         
-        jest.runAllTimers()
-
-        //expect(nextInterval).toBe(10000)
-        expect(callback).toHaveBeenCalledTimes(4)
-    })*/
+        // After 1 second
+        jest.advanceTimersByTime(1000)
+        expect(nextInterval).toBe(2000)
+        expect(callback).toHaveBeenCalledTimes(1)
+        // After 2 seconds
+        jest.advanceTimersByTime(1000)
+        expect(nextInterval).toBe(2000)
+        expect(callback).toHaveBeenCalledTimes(1)
+        // After 3 seconds
+        jest.advanceTimersByTime(1000)
+        expect(nextInterval).toBe(3000)
+        expect(callback).toHaveBeenCalledTimes(2)
+        // After 4 seconds
+        jest.advanceTimersByTime(1000)
+        expect(nextInterval).toBe(3000)
+        expect(callback).toHaveBeenCalledTimes(2)
+        // After 5 seconds
+        jest.advanceTimersByTime(1000)
+        expect(nextInterval).toBe(3000)
+        expect(callback).toHaveBeenCalledTimes(2)
+        // After 6 seconds
+        jest.advanceTimersByTime(1000)
+        expect(nextInterval).toBe(4000)
+        expect(callback).toHaveBeenCalledTimes(3)
+        // After 7 seconds
+        jest.advanceTimersByTime(1000)
+        expect(nextInterval).toBe(4000)
+        expect(callback).toHaveBeenCalledTimes(3)
+        // After 8 seconds
+        jest.advanceTimersByTime(1000)
+        expect(nextInterval).toBe(4000)
+        expect(callback).toHaveBeenCalledTimes(3)
+        // After 9 seconds
+        jest.advanceTimersByTime(1000)
+        expect(nextInterval).toBe(4000)
+        expect(callback).toHaveBeenCalledTimes(3)
+        // After 10 seconds: STOP
+        jest.advanceTimersByTime(1000)
+        expect(nextInterval).toBe(4000)
+        expect(callback).toHaveBeenCalledTimes(3)
+    })
 })
